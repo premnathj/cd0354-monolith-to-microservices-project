2a11921bae3b4157ddc3db503d1d1ac42b3fb16a
Prem <premnathj@gmail.com>
2023-05-22 17:18:09 +0530

    added k8s yaml files

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a750cd09824ab419994d0d8a8c2ce41d-2108462290.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
